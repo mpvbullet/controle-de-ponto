848fb920124212baf435d79d8c2608d0258dad9f
Marcos Paulo Dias Teixeira <marcospaulo.uba@gmail.com>
2018-07-08 01:02:04 -0300

    :pencil: Adiciona artefatos para conexão com o banco de dados PostgreSQL e o JPA

--- src/main/java/br/com/syntax/controledeponto/ControleDePontoApplication.java
+++ src/main/java/br/com/syntax/controledeponto/ControleDePontoApplication.java
@@ -1,4 +1,4 @@
-package br.com.syntax.pontointeligente;
+package br.com.syntax.controledeponto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/syntax/controledeponto/ControleDePontoApplicationTests.java
+++ src/test/java/br/com/syntax/controledeponto/ControleDePontoApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.syntax.pontointeligente;
+package br.com.syntax.controledeponto;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
