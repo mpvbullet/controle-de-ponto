def86f3c34ae54444bde890d437fd5d3f4f133e5
Marcos Paulo Dias Teixeira <marcospaulo.uba@gmail.com>
2018-07-08 03:24:55 -0300

    :sparkles: Adiciona o enum com os tipos de controle de ponto que serão controlados pelo projeto

--- src/main/java/br/com/syntax/controledeponto/enums/TipoDeControle.java
+++ src/main/java/br/com/syntax/controledeponto/enums/TipoDeControle.java
@@ -1,4 +1,12 @@
 package br.com.syntax.controledeponto.enums;
 
 public enum TipoDeControle {
+
+  INICIO_TRABALHO,
+  TERMINO_TRABALHO,
+  INICIO_ALMOCO,
+  TERMINO_ALMOCO,
+  INICIO_PAUSA,
+  TERMINO_PAUSA;
+
 }
