03ce9d6bf9bdec563d4b3e9ff3108fe23872111a
Marcos Paulo Dias Teixeira <marcospaulo.uba@gmail.com>
2018-07-08 21:24:45 -0300

    :sparkles: feat(ControleDePontoApplicationTest) : Configura classe main de testes

--- src/test/java/br/com/syntax/controledeponto/ControleDePontoApplicationTests.java
+++ src/test/java/br/com/syntax/controledeponto/ControleDePontoApplicationTests.java
@@ -3,10 +3,12 @@ package br.com.syntax.controledeponto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test") // Para utilizar o application-test.properties
 public class ControleDePontoApplicationTests {
 
     @Test
