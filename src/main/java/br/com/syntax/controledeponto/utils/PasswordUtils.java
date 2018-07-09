d05c6ace5b6f3e41ce114181c4048a6fea84c4df
Marcos Paulo Dias Teixeira <marcospaulo.uba@gmail.com>
2018-07-08 21:29:52 -0300

    :sparkles: feat(PasswordUtils) : Classe para manipular a senha através o BCrypt

--- src/main/java/br/com/syntax/controledeponto/utils/PasswordUtils.java
+++ src/main/java/br/com/syntax/controledeponto/utils/PasswordUtils.java
@@ -1,4 +1,20 @@
 package br.com.syntax.controledeponto.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class PasswordUtils {
+
+  private static final Logger log = LoggerFactory.getLogger(PasswordUtils.class);
+
+  public static String geraBCrypt(String senha) {
+    if (senha == null) {
+      return senha;
+    }
+
+    log.debug("Gerando hash da senha com BCrypt");
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    return encoder.encode(senha);
+  }
 }
