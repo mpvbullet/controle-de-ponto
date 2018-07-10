93b0494ae00a676f9b72a5a6b6b42c9d544b1fb2
Marcos Paulo Dias Teixeira <marcospaulo.uba@gmail.com>
2018-07-09 23:42:35 -0300

    :sparkles: feat(Response) : Cria classe padrão para retornar a resposta da requisição na api

--- src/main/java/br/com/syntax/controledeponto/api/response/Response.java
+++ src/main/java/br/com/syntax/controledeponto/api/response/Response.java
@@ -1,4 +1,30 @@
 package br.com.syntax.controledeponto.api.response;
 
-public class Response {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Response<T> {
+
+  private T retorno;
+  private List<String> erros;
+
+  public T getRetorno() {
+    return retorno;
+  }
+
+  public void setRetorno(T retorno) {
+    this.retorno = retorno;
+  }
+
+  public List<String> getErros() {
+    if (this.erros == null) {
+      this.erros = new ArrayList<>();
+    }
+    return this.erros;
+  }
+
+  public void setErros(List<String> erros) {
+    this.erros = erros;
+  }
+
 }
