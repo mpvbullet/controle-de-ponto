2b6fe214a2c9e6e511acf2284e1084367c5d3a99
Marcos Paulo Dias Teixeira <marcospaulo.uba@gmail.com>
2018-07-10 01:44:53 -0300

    :sparkles: feat(PessoaFisicaDto) : Cria dto para trafegar os dados de pessoa física

--- src/main/java/br/com/syntax/controledeponto/api/dtos/PessoaFisicaDto.java
+++ src/main/java/br/com/syntax/controledeponto/api/dtos/PessoaFisicaDto.java
@@ -1,5 +1,11 @@
-package br.com.syntax.controledeponto.api.controllers;
+package br.com.syntax.controledeponto.api.dtos;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.math.BigDecimal;
 
 public class PessoaFisicaDto {
@@ -24,6 +30,8 @@ public class PessoaFisicaDto {
     this.id = id;
   }
 
+  @NotEmpty(message = "O nome não pode ser vazio.")
+  @Length(min = 3, max = 200, message = "O nome deve conter entre 3 e 200 caracteres")
   public String getNome() {
     return nome;
   }
@@ -32,6 +40,8 @@ public class PessoaFisicaDto {
     this.nome = nome;
   }
 
+  @NotEmpty(message = "O CPF não pode ser vazio.")
+  @CPF(message = "O CPF está inválido")
   public String getCpf() {
     return cpf;
   }
@@ -40,6 +50,9 @@ public class PessoaFisicaDto {
     this.cpf = cpf;
   }
 
+  @NotEmpty(message = "O email não pode ser vazio.")
+  @Length(min = 10, max = 200, message = "O email deve conter entre 10 e 200 caracteres")
+  @Email(message = "Email inválido")
   public String getEmail() {
     return email;
   }
@@ -48,6 +61,8 @@ public class PessoaFisicaDto {
     this.email = email;
   }
 
+  @NotEmpty(message = "A senha não pode ser vazia.")
+  @Length(min = 6, message = "A senha deve conter no mínimo 6 caracteres")
   public String getSenha() {
     return senha;
   }
@@ -80,9 +95,12 @@ public class PessoaFisicaDto {
     this.quantidadeHorasAlmoco = quantidadeHorasAlmoco;
   }
 
+  @NotEmpty(message = "O CNPJ não pode ser vazio.")
+  @CNPJ(message = "O CNPJ está inválido")
   public String getCnpj() {
     return cnpj;
   }
+
 
   public void setCnpj(String cnpj) {
     this.cnpj = cnpj;
