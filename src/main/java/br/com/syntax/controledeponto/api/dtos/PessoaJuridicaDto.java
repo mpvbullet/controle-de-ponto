8142ec9667616e4f2b49ef21b29245bfa46ae4a0
Marcos Paulo Dias Teixeira <marcospaulo.uba@gmail.com>
2018-07-10 23:23:42 -0300

    :sparkles: feat(PessoaJuridicaDto) : Cria DTO para trafegar dados de pessoa jurídica

--- src/main/java/br/com/syntax/controledeponto/api/dtos/PessoaJuridicaDto.java
+++ src/main/java/br/com/syntax/controledeponto/api/dtos/PessoaJuridicaDto.java
@@ -1,4 +1,32 @@
 package br.com.syntax.controledeponto.api.dtos;
 
 public class PessoaJuridicaDto {
+
+  private Long id;
+  private String razaoSocial;
+  private String cnpj;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getRazaoSocial() {
+    return razaoSocial;
+  }
+
+  public void setRazaoSocial(String razaoSocial) {
+    this.razaoSocial = razaoSocial;
+  }
+
+  public String getCnpj() {
+    return cnpj;
+  }
+
+  public void setCnpj(String cnpj) {
+    this.cnpj = cnpj;
+  }
 }
