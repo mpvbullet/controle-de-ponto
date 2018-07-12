39358e1e6136c39be9d0f07b4e0618a80da6ef18
Marcos Paulo Dias Teixeira <marcospaulo.uba@gmail.com>
2018-07-12 16:52:56 -0300

    :sparkles: feat(ControleDePontoApplication): Habilita do EhCache

--- src/main/java/br/com/syntax/controledeponto/ControleDePontoApplication.java
+++ src/main/java/br/com/syntax/controledeponto/ControleDePontoApplication.java
@@ -2,8 +2,10 @@ package br.com.syntax.controledeponto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ControleDePontoApplication {
 
     public static void main(String[] args) {
